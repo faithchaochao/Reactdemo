aae59b2281439ec27fb8baead94a827fde54becc
faithchaochao <19831122090@163.com>
2020-10-02 10:21:30 +0800

    message

---
+++ build/precache-manifest.ff6bcf7f6aba85b3c7cc907dfda5defe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d68e2d29ee504c5837a9ad102b3cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f48f338cf8ae4ccf411",
+    "url": "/static/css/main.5c2fe313.chunk.css"
+  },
+  {
+    "revision": "bc59455f82df65bc9e95",
+    "url": "/static/js/2.39a7d7a7.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.39a7d7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f48f338cf8ae4ccf411",
+    "url": "/static/js/main.b211100c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
